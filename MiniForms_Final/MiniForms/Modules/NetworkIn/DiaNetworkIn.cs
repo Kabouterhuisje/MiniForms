b93ce7313ddb858e482fc4062f5a8cd75e060574
Dennis Tijbosch <d.tijbosch@yahoo.com>
2016-02-25 16:28:53 +0100

    MiniForms added
    Framework with modules

---
+++ MiniForms_Final/MiniForms/Modules/NetworkIn/DiaNetworkIn.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace MiniForms.Modules.NetworkIn
+{
+    public partial class DiaNetworkIn : Form
+    {
+        private readonly NetworkIn _networkIn;
+        public DiaNetworkIn(NetworkIn networkIn)
+        {
+            InitializeComponent();
+            _networkIn = networkIn;
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            _networkIn.Port = txtPort.Text;
+            Close();
+        }
+
+        private void DiaNetworkIn_Load(object sender, EventArgs e)
+        {
+            if (_networkIn.Port != null)
+                txtPort.Text = _networkIn.Port;
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
